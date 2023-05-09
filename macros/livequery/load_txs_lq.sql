4e4cd4bee42b44b82ad7a15724a148508c71cd80
Eric Laurello <eric.laurello@flipsidecrypto.com>
2023-05-09 13:36:02 -0400

    .

--- macros/livequery/load_txs_lq.sql
+++ macros/livequery/load_txs_lq.sql
@@ -64,7 +64,7 @@ FROM
 {% endset %}
     {% do run_query(load_query) %}
     {% set wait %}
-    CALL system $ wait(10);
+    CALL system$wait(10);
 {% endset %}
     {% do run_query(wait) %}
 {% endmacro %}
