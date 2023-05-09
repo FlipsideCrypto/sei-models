593d71cf69dce538e7ce0a2107964a04bbe890c0
eric-laurello <102970824+eric-laurello@users.noreply.github.com>
2023-05-09 15:49:37 -0400

    Update load_txs_lq.sql

--- macros/livequery/load_txs_lq.sql
+++ macros/livequery/load_txs_lq.sql
@@ -13,7 +13,7 @@ INSERT INTO
             (
                 SELECT
                     *,
-                    NTILE (10) over(PARTITION BY getdate()
+                    NTILE (30) over(PARTITION BY getdate()
                 ORDER BY
                     block_number) AS grp
                 FROM
@@ -30,7 +30,7 @@ INSERT INTO
                         ORDER BY
                             1 DESC
                         LIMIT
-                            250
+                            750
                     )
             )
         GROUP BY
