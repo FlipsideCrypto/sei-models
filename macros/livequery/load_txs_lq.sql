8485c6936e8c8f8d7a52edd6aa641982b944c074
Eric Laurello <eric.laurello@flipsidecrypto.com>
2023-05-11 08:19:55 -0400

    tuning

--- macros/livequery/load_txs_lq.sql
+++ macros/livequery/load_txs_lq.sql
@@ -13,7 +13,7 @@ INSERT INTO
             (
                 SELECT
                     *,
-                    NTILE (40) over(PARTITION BY getdate()
+                    NTILE (200) over(PARTITION BY getdate()
                 ORDER BY
                     block_number) AS grp
                 FROM
@@ -30,7 +30,7 @@ INSERT INTO
                         ORDER BY
                             1 DESC
                         LIMIT
-                            750
+                            2000
                     )
             )
         GROUP BY
