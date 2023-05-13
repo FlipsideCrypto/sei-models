f3c906e546976bb04b72db7e6c1e438dacea8e01
Eric Laurello <eric.laurello@flipsidecrypto.com>
2023-05-13 07:59:32 -0400

    slow

--- macros/livequery/load_blocks_lq.sql
+++ macros/livequery/load_blocks_lq.sql
@@ -66,7 +66,7 @@ INSERT INTO
                         ORDER BY
                             1 DESC
                         LIMIT
-                            2000
+                            1500
                     )
             )
         GROUP BY
