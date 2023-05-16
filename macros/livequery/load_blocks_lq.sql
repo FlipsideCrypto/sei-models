65c74f68a784aad3d6d3f6d43265ba89434dd86b
Eric Laurello <eric.laurello@flipsidecrypto.com>
2023-05-16 10:08:05 -0400

    tennnn

--- macros/livequery/load_blocks_lq.sql
+++ macros/livequery/load_blocks_lq.sql
@@ -49,7 +49,7 @@ INSERT INTO
             (
                 SELECT
                     *,
-                    NTILE (200) over(PARTITION BY getdate()
+                    NTILE (10) over(PARTITION BY getdate()
                 ORDER BY
                     block_height) AS grp
                 FROM
@@ -66,7 +66,7 @@ INSERT INTO
                         ORDER BY
                             1 DESC
                         LIMIT
-                            200
+                            10
                     )
             )
         GROUP BY
