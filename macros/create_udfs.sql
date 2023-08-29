1cfa0d3b5e16b928d12403750a7b6fd30e9fb08d
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2023-08-29 12:26:28 -0400

    Sei txs, blocks, tx count pipeline (#2)
    * Sei txs, blocks, tx count pipeline
    
    * Streamline sei pipelines
    
    - XXX will be deleted with PROD deployment after approvals
    - Temp file will be deleted after UDFs are created
    
    * Add requirement and update block_number to block_id
    
    * Switch to block number
    
    * Remove old sources
    
    * Update loads and run times
    
    * Add prod integration

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -1,2 +1,14 @@
 {% macro create_udfs() %}
+    {% if var("UPDATE_UDFS_AND_SPS") %}
+        {% set sql %}
+        CREATE schema if NOT EXISTS silver;
+{{ create_udtf_get_base_table(
+            schema = "streamline"
+        ) }}
+        {{ create_udf_get_chainhead() }}
+        {{ create_udf_bulk_json_rpc() }}
+
+        {% endset %}
+        {% do run_query(sql) %}
+    {% endif %}
 {% endmacro %}
