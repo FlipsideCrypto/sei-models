0efdfb02bf377a1ed15c2085b92973ca39064f73
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2023-08-29 12:24:57 -0400

    Add prod integration

--- macros/streamline/api_integrations.sql
+++ macros/streamline/api_integrations.sql
@@ -5,8 +5,8 @@
 
     {% if target.name == "prod" %}
         {% set sql %}
-        CREATE api integration IF NOT EXISTS aws_sei_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::490041342817:role/gnosis-api-prod-rolesnowflakeudfsAF733095-1S5YHE2BPGRAE' api_allowed_prefixes = (
-            'https://xxx.execute-api.us-east-1.amazonaws.com/prod/'
+        CREATE api integration IF NOT EXISTS aws_sei_api api_provider = aws_api_gateway api_aws_role_arn = 'arn:aws:iam::490041342817:role/sei-api-prod-rolesnowflakeudfsAF733095-CZPTGU57XEA0' api_allowed_prefixes = (
+            'https://v19hb3dk4k.execute-api.us-east-1.amazonaws.com/prod/'
         ) enabled = TRUE;
 {% endset %}
         {% do run_query(sql) %}
--- macros/streamline/streamline_udfs.sql
+++ macros/streamline/streamline_udfs.sql
@@ -2,7 +2,7 @@
     CREATE
     OR REPLACE EXTERNAL FUNCTION streamline.udf_get_chainhead() returns variant api_integration =
     {% if target.name == "prod" %}
-        aws_sei_api AS 'https://xxx.execute-api.us-east-1.amazonaws.com/prod/get_chainhead'
+        aws_sei_api AS 'https://v19hb3dk4k.execute-api.us-east-1.amazonaws.com/prod/get_chainhead'
     {% else %}
         aws_sei_api_dev AS 'https://u1hda5gxml.execute-api.us-east-1.amazonaws.com/dev/get_chainhead'
     {%- endif %};
@@ -13,7 +13,7 @@
     OR REPLACE EXTERNAL FUNCTION streamline.udf_bulk_json_rpc(
         json variant
     ) returns text api_integration = {% if target.name == "prod" %}
-        aws_sei_api AS 'https://xxx.execute-api.us-east-1.amazonaws.com/prod/udf_bulk_json_rpc'
+        aws_sei_api AS 'https://v19hb3dk4k.execute-api.us-east-1.amazonaws.com/prod/udf_bulk_json_rpc'
     {% else %}
         aws_sei_api_dev AS 'https://u1hda5gxml.execute-api.us-east-1.amazonaws.com/dev/udf_bulk_json_rpc'
     {%- endif %};
