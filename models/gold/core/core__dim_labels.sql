a2ce92d752702fc9746650d3c406124f696f1a16
Eric Laurello <eric.laurello@flipsidecrypto.com>
2024-01-30 10:16:42 -0500

    An-4465 labels

--- models/gold/core/core__dim_labels.sql
+++ models/gold/core/core__dim_labels.sql
@@ -11,19 +11,8 @@ SELECT
     label_subtype,
     project_name AS label,
     address_name AS address_name,
-    COALESCE (
-        crosschain_labels_id,
-        {{ dbt_utils.generate_surrogate_key(
-            ['address']
-        ) }}
-    ) AS dim_labels_id,
-    COALESCE(
-        inserted_timestamp,
-        '2000-01-01'
-    ) AS inserted_timestamp,
-    COALESCE(
-        modified_timestamp,
-        '2000-01-01'
-    ) AS modified_timestamp
+    labels_combined_id AS dim_labels_id,
+    inserted_timestamp,
+    modified_timestamp
 FROM
     {{ ref('silver__croschain_labels') }}
