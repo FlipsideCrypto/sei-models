d03290980011b1ff4ffd77c7e9021f66dcd35fae
eric-laurello <102970824+eric-laurello@users.noreply.github.com>
2024-04-25 09:25:12 -0400

    updates to external table and views (#13)

--- models/bronze/core/streamline/bronze__streamline_FR_blocks_v2.sql
+++ models/bronze/core/streamline/bronze__streamline_FR_blocks_v2.sql
@@ -1,9 +1,7 @@
 {{ config (
     materialized = 'view'
 ) }}
-{{ streamline_external_table_FR_query(
-    "blocks_v2",
-    partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 4), '_', 1) AS INTEGER )",
-    partition_name = "_partition_by_block_id",
-    unique_key = "partition_key"
+{{ streamline_external_table_FR_query_v2(
+    model = "blocks_v2",
+    partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 4), '_', 1) AS INTEGER )"
 ) }}
--- models/bronze/core/streamline/bronze__streamline_FR_transactions_v2.sql
+++ models/bronze/core/streamline/bronze__streamline_FR_transactions_v2.sql
@@ -1,9 +1,7 @@
 {{ config (
     materialized = 'view'
 ) }}
-{{ streamline_external_table_FR_query(
+{{ streamline_external_table_FR_query_v2(
     model = "transactions_v2",
-    partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 4), '_', 1) AS INTEGER )",
-    partition_name = "_partition_by_block_id",
-    unique_key = "partition_key"
+    partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 4), '_', 1) AS INTEGER )"
 ) }}
