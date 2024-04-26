f5beaa132b1adb25458598a02d4b27428f1a5cc4
Austin <austin@flipsidecrypto.com>
2024-04-26 10:02:21 -0400

    params

--- models/evm/streamline/silver/realtime/streamline__evm_traces_realtime.sql
+++ models/evm/streamline/silver/realtime/streamline__evm_traces_realtime.sql
@@ -4,9 +4,9 @@
         func = 'streamline.udf_bulk_rest_api_v2',
         target = "{{this.schema}}.{{this.identifier}}",
         params ={ "external_table" :"evm_traces_testnet",
-        "sql_limit" :"100000",
-        "producer_batch_size" :"100000",
-        "worker_batch_size" :"10000",
+        "sql_limit" :"10000",
+        "producer_batch_size" :"1000",
+        "worker_batch_size" :"10",
         "sql_source" :"{{this.identifier}}",
         "exploded_key": "[\"result\"]" }
     ),
