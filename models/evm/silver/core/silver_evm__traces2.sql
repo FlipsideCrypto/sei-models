537d30af324e5ee3a15f6241dd3868f11dd3121a
Austin <93135983+austinFlipside@users.noreply.github.com>
2024-06-26 13:04:28 -0400

    An 4953/regular logic (#36)
    * regular logic
    
    * tx hash

--- models/evm/silver/core/silver_evm__traces2.sql
+++ models/evm/silver/core/silver_evm__traces2.sql
@@ -5,7 +5,8 @@
     unique_key = "block_number",
     cluster_by = ['modified_timestamp::DATE','partition_key'],
     post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION",
-    tags = ['traces2']
+    tags = ['core'],
+    full_refresh = false
 ) }}
 
 WITH bronze_traces AS (
@@ -20,17 +21,11 @@ WITH bronze_traces AS (
     FROM
 
 {% if is_incremental() %}
-{{ ref('bronze_evm__streamline_FR_traces') }}
+{{ ref('bronze_evm__streamline_traces') }}
 WHERE
-    partition_key BETWEEN (
+    _inserted_timestamp >= (
         SELECT
-            MAX(partition_key)
-        FROM
-            {{ this }}
-    )
-    AND (
-        SELECT
-            MAX(partition_key) + 1000000
+            MAX(_inserted_timestamp) _inserted_timestamp
         FROM
             {{ this }}
     )
