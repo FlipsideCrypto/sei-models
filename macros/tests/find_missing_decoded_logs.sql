c7a7fff687445e38c6171c9547b3e606902c7228
Austin <93135983+austinFlipside@users.noreply.github.com>
2024-11-05 10:31:34 -0500

    fixes (#70)

--- macros/tests/find_missing_decoded_logs.sql
+++ macros/tests/find_missing_decoded_logs.sql
@@ -1,13 +1,13 @@
 {% test find_missing_decoded_logs(model, fact_logs_model) %}
 SELECT
     l.block_number,
-    l.fact_event_logs_id
+    l.logs_id
 FROM
     {{ fact_logs_model }}
     l
     LEFT JOIN {{ model }}
     d
-    ON d.ez_decoded_event_logs_id = l.fact_event_logs_id
+    ON d.ez_decoded_event_logs_id = l.logs_id
 WHERE
     l.contract_address = lower('0xE30feDd158A2e3b13e9badaeABaFc5516e95e8C7')
     AND l.topics [0] :: STRING = '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef' -- Transfer
