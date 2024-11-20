c698f792254f6532469de0bee784a858be4ce652
Austin <austin@flipsidecrypto.com>
2024-11-20 09:59:29 -0500

    fix

--- models/evm/bronze/api_udf/bronze_evm_api__contract_abis.sql
+++ models/evm/bronze/api_udf/bronze_evm_api__contract_abis.sql
@@ -15,11 +15,14 @@ WITH base AS (
         total_interaction_count >= 100
 
 {% if is_incremental() %}
-EXCEPT
+and contract_address not in (
 SELECT
     contract_address
 FROM
     {{ this }}
+    WHERE
+        abi_data :data :result :: STRING <> 'Max rate limit reached'
+)
 {% endif %}
 ORDER BY
     total_interaction_count DESC
