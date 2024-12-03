42c07f513c727c1f694c13351e228cdcb138054f
Eric Laurello <eric.laurello@flipsidecrypto.com>
2024-12-03 15:35:54 -0500

    coalesce

--- models/streamline/core/complete/streamline__complete_sei_addresses.sql
+++ models/streamline/core/complete/streamline__complete_sei_addresses.sql
@@ -24,14 +24,13 @@ WHERE
     DATA :result LIKE 'sei%'
 
 {% if is_incremental() %}
-AND _inserted_timestamp >= (
+AND _inserted_timestamp >=(
     SELECT
-        MAX(_inserted_timestamp)
+        COALESCE(MAX(_inserted_timestamp), '1970-01-01' :: TIMESTAMP) _inserted_timestamp
     FROM
-        {{ this }}
-)
-{% endif %}
+        {{ this }})
+    {% endif %}
 
-qualify(ROW_NUMBER() over (PARTITION BY evm_address
-ORDER BY
-    _inserted_timestamp DESC)) = 1
+    qualify(ROW_NUMBER() over (PARTITION BY evm_address
+    ORDER BY
+        _inserted_timestamp DESC)) = 1
