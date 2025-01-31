26a0b91a581cf87b72b63626356209e9b31b9d11
Austin <93135983+austinFlipside@users.noreply.github.com>
2025-01-31 08:46:57 -0500

    silver (#94)

--- models/evm/silver/core/tests/transactions/test_silver_evm__transactions_recent.sql
+++ models/evm/silver/core/tests/transactions/test_silver_evm__transactions_recent.sql
@@ -3,22 +3,14 @@
     tags = ['recent_evm_test']
 ) }}
 
-WITH last_3_days AS (
-
-    SELECT
-        block_number
-    FROM
-        {{ ref("_evm_block_lookback") }}
-)
 SELECT
     *
 FROM
     {{ ref('silver_evm__transactions') }}
 WHERE
-    block_number >= (
+    block_number > (
         SELECT
             block_number
         FROM
-            last_3_days
+            {{ ref("_evm_block_lookback") }}
     )
-    AND block_timestamp < DATEADD('hour', -1, SYSDATE())
