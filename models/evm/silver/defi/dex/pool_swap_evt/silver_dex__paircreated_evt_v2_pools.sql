741dd7e0aa65729f5312acf9bd564bde85d07613
drethereum <71602799+drethereum@users.noreply.github.com>
2025-08-11 10:50:35 -0600

    AN-6482/sei-evm-dex (#119)
    * temp dex
    
    * remove order book dex oxium and update vars
    
    * is verified in prices
    
    * gold models
    
    * tags and remove legacy models
    
    * heal wf
    
    * remove file
    
    * revert
    
    * remove event name
    
    * remove so

---
+++ models/evm/silver/defi/dex/pool_swap_evt/silver_dex__paircreated_evt_v2_pools.sql
@@ -0,0 +1,84 @@
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'delete+insert',
+    unique_key = 'pool_address',
+    tags = ['silver_dex','defi','dex','curated']
+) }}
+
+WITH contract_mapping AS (
+    SELECT * FROM VALUES
+        ('0x595a6b190ba0163eac1da988955563635ab3aa60', 'uniswap', 'v2', 'uni_v2_pair_created'),
+        ('0x4b4746216214f9e972c5d35d3fe88e6ec4c28a6b', 'donkeswap', 'v2', 'uni_v2_pair_created'),
+        ('0x5d370a6189f89603fab67e9c68383e63f7b6a262', 'dragonswap', 'v1', 'uni_v2_pair_created'),
+        ('0x71b27deea549cf3e9293544b2af7a193a34be29a', 'dragonswap', 'v1', 'uni_v2_pair_created')
+    AS t(contract_address, protocol, version, type)
+),
+pools AS (
+    SELECT
+        block_number,
+        block_timestamp,
+        tx_hash,
+        event_index,
+        l.contract_address,
+        regexp_substr_all(SUBSTR(DATA, 3, len(DATA)), '.{64}') AS segmented_data,
+        CONCAT('0x', SUBSTR(topics [1] :: STRING, 27, 40)) AS token0,
+        CONCAT('0x', SUBSTR(topics [2] :: STRING, 27, 40)) AS token1,
+        CONCAT('0x', SUBSTR(segmented_data [0] :: STRING, 25, 40)) AS pool_address,
+        utils.udf_hex_to_int(
+            segmented_data [1] :: STRING
+        ) :: INT AS pool_id,
+        m.protocol,
+        m.version,
+        m.type,
+        CONCAT(
+            m.protocol,
+            '-',
+            m.version
+        ) AS platform,
+        'PairCreated' AS event_name,
+        CONCAT(
+            tx_hash :: STRING,
+            '-',
+            event_index :: STRING
+        ) AS _log_id,
+        modified_timestamp
+    FROM
+        {{ ref('core_evm__fact_event_logs') }}
+        l
+        INNER JOIN contract_mapping m
+        ON l.contract_address = m.contract_address
+    WHERE
+        topics [0] :: STRING = '0x0d3648bd0f6ba80134a33ba9275ac585d9d315f0ad8355cddefde31afa28d0e9' --PairCreated
+        AND tx_succeeded
+
+{% if is_incremental() %}
+AND modified_timestamp >= (
+    SELECT
+        MAX(modified_timestamp) - INTERVAL '12 hours'
+    FROM
+        {{ this }}
+)
+AND modified_timestamp >= SYSDATE() - INTERVAL '7 days'
+{% endif %}
+)
+SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    contract_address,
+    event_index,
+    event_name,
+    token0,
+    token1,
+    pool_address,
+    pool_id,
+    platform,
+    protocol,
+    version,
+    type,
+    _log_id,
+    modified_timestamp
+FROM
+    pools qualify(ROW_NUMBER() over (PARTITION BY pool_address
+ORDER BY
+    modified_timestamp DESC)) = 1
---
+++ models/evm/silver/defi/dex/silver_dex__complete_dex_swaps.sql
@@ -0,0 +1,699 @@
+-- depends_on: {{ ref('price__ez_asset_metadata') }}
+
+{{ config(
+  materialized = 'incremental',
+  incremental_strategy = 'delete+insert',
+  unique_key = ['block_number','platform','version'],
+  cluster_by = ['block_timestamp::DATE','platform'],
+  post_hook = "ALTER TABLE {{ this }} ADD SEARCH OPTIMIZATION ON EQUALITY(tx_hash, origin_function_signature, origin_from_address, origin_to_address, sender, tx_to, token_in, token_out, symbol_in, symbol_out)",
+  tags = ['silver_dex','defi','dex','curated','heal','complete','swap']
+) }}
+
+WITH contracts AS (
+
+  SELECT
+    address AS contract_address,
+    symbol AS token_symbol,
+    decimals AS token_decimals,
+    modified_timestamp AS _inserted_timestamp
+  FROM
+    {{ ref('core_evm__dim_contracts') }}
+  UNION ALL
+  SELECT
+    '0x0000000000000000000000000000000000000000' AS contract_address,
+    'SEI' AS token_symbol,
+    decimals AS token_decimals,
+    modified_timestamp AS _inserted_timestamp
+  FROM
+    {{ ref('core_evm__dim_contracts') }}
+  WHERE
+    address = '0xe30fedd158a2e3b13e9badaeabafc5516e95e8c7'
+),
+prices AS (
+  SELECT
+    token_address,
+    price,
+    HOUR,
+    is_verified,
+    modified_timestamp AS _inserted_timestamp
+  FROM
+    {{ ref('price__ez_prices_hourly') }}
+  UNION ALL
+  SELECT
+    '0x0000000000000000000000000000000000000000' AS token_address,
+    price,
+    HOUR,
+    is_verified,
+    modified_timestamp AS _inserted_timestamp
+  FROM
+    {{ ref('price__ez_prices_hourly') }}
+  WHERE
+    token_address = '0xe30fedd158a2e3b13e9badaeabafc5516e95e8c7'
+),
+swap_evt_v3 AS (
+
+  SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    origin_function_signature,
+    origin_from_address,
+    origin_to_address,
+    pool_address AS contract_address,
+    '0x' AS pool_id,
+    event_name,
+    amount_in_unadj,
+    amount_out_unadj,
+    token_in,
+    token_out,
+    sender,
+    recipient AS tx_to,
+    event_index,
+    platform,
+    protocol,
+    version,
+    type,
+    _log_id,
+    modified_timestamp AS _inserted_timestamp
+  FROM
+    {{ ref('silver_dex__swap_evt_v3_swaps') }}
+
+{% if is_incremental() and 'swap_evt_v3' not in var('HEAL_MODELS') %}
+WHERE
+  _inserted_timestamp >= (
+    SELECT
+      MAX(_inserted_timestamp) - INTERVAL '{{ var("LOOKBACK", "4 hours") }}'
+    FROM
+      {{ this }}
+  )
+{% endif %}
+),
+swap_evt_v2 AS (
+  SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    origin_function_signature,
+    origin_from_address,
+    origin_to_address,
+    contract_address,
+    '0x' AS pool_id,
+    event_name,
+    amount_in_unadj,
+    amount_out_unadj,
+    token_in,
+    token_out,
+    sender,
+    tx_to,
+    event_index,
+    platform,
+    protocol,
+    version,
+    type,
+    _log_id,
+    modified_timestamp AS _inserted_timestamp
+  FROM
+    {{ ref('silver_dex__swap_evt_v2_swaps') }}
+
+{% if is_incremental() and 'swap_evt_v2' not in var('HEAL_MODELS') %}
+WHERE
+  _inserted_timestamp >= (
+    SELECT
+      MAX(_inserted_timestamp) - INTERVAL '{{ var("LOOKBACK", "4 hours") }}'
+    FROM
+      {{ this }}
+  )
+{% endif %}
+),
+jellyswap AS (
+  SELECT
+    block_number,
+    block_timestamp,
+    tx_hash,
+    origin_function_signature,
+    origin_from_address,
+    origin_to_address,
+    contract_address,
+    '0x' AS pool_id,
+    event_name,
+    amount_in_unadj,
+    amount_out_unadj,
+    token_in,
+    token_out,
+    sender,
+    tx_to,
+    event_index,
+    platform,
+    protocol,
+    version,
+    type,
+    _log_id,
+    _inserted_timestamp
+  FROM
+    {{ ref('silver_dex__jellyswap_swaps') }}
+
+{% if is_incremental() and 'jellyswap' not in var('HEAL_MODELS') %}
+WHERE
+  _inserted_timestamp >= (
+    SELECT
+      MAX(_inserted_timestamp) - INTERVAL '{{ var("LOOKBACK", "4 hours") }}'
+    FROM
+      {{ this }}
+  )
+{% endif %}
+),
+all_dex AS (
+  SELECT
+    *
+  FROM
+    jellyswap
+  UNION ALL
+  SELECT
+    *
+  FROM
+    swap_evt_v3
+  UNION ALL
+  SELECT
+    *
+  FROM
+    swap_evt_v2
+),
+complete_dex_swaps AS (
+  SELECT
+    s.block_number,
+    s.block_timestamp,
+    s.tx_hash,
+    origin_function_signature,
+    origin_from_address,
+    origin_to_address,
+    s.contract_address,
+    s.pool_id,
+    event_name,
+    token_in,
+    p1.is_verified AS token_in_is_verified,
+    c1.token_decimals AS decimals_in,
+    c1.token_symbol AS symbol_in,
+    amount_in_unadj,
+    CASE
+      WHEN decimals_in IS NULL THEN amount_in_unadj
+      ELSE (amount_in_unadj / pow(10, decimals_in))
+    END AS amount_in,
+    CASE
+      WHEN decimals_in IS NOT NULL THEN amount_in * p1.price
+      ELSE NULL
+    END AS amount_in_usd,
+    token_out,
+    p2.is_verified AS token_out_is_verified,
+    c2.token_decimals AS decimals_out,
+    c2.token_symbol AS symbol_out,
+    amount_out_unadj,
+    CASE
+      WHEN decimals_out IS NULL THEN amount_out_unadj
+      ELSE (amount_out_unadj / pow(10, decimals_out))
+    END AS amount_out,
+    CASE
+      WHEN decimals_out IS NOT NULL THEN amount_out * p2.price
+      ELSE NULL
+    END AS amount_out_usd,
+    CASE
+      WHEN lp.pool_name IS NULL THEN CONCAT(
+        LEAST(
+          COALESCE(
+            symbol_in,
+            CONCAT(SUBSTRING(token_in, 1, 5), '...', SUBSTRING(token_in, 39, 42))
+          ),
+          COALESCE(
+            symbol_out,
+            CONCAT(SUBSTRING(token_out, 1, 5), '...', SUBSTRING(token_out, 39, 42))
+          )
+        ),
+        '-',
+        GREATEST(
+          COALESCE(
+            symbol_in,
+            CONCAT(SUBSTRING(token_in, 1, 5), '...', SUBSTRING(token_in, 39, 42))
+          ),
+          COALESCE(
+            symbol_out,
+            CONCAT(SUBSTRING(token_out, 1, 5), '...', SUBSTRING(token_out, 39, 42))
+          )
+        )
+      )
+      ELSE lp.pool_name
+    END AS pool_name,
+    sender,
+    tx_to,
+    event_index,
+    s.platform,
+    s.protocol,
+    s.version,
+    s.type,
+    s._log_id,
+    s._inserted_timestamp
+  FROM
+    all_dex s
+    LEFT JOIN contracts
+    c1
+    ON s.token_in = c1.contract_address
+    LEFT JOIN contracts
+    c2
+    ON s.token_out = c2.contract_address
+    LEFT JOIN prices
+    p1
+    ON s.token_in = p1.token_address
+    AND DATE_TRUNC(
+      'hour',
+      block_timestamp
+    ) = p1.hour
+    LEFT JOIN prices
+    p2
+    ON s.token_out = p2.token_address
+    AND DATE_TRUNC(
+      'hour',
+      block_timestamp
+    ) = p2.hour
+    LEFT JOIN {{ ref('silver_dex__complete_dex_liquidity_pools') }}
+    lp
+    ON s.contract_address = lp.pool_address
+    AND s.pool_id = lp.pool_id
+),
+
+{% if is_incremental() and var(
+  'HEAL_MODEL'
+) %}
+heal_model AS (
+  SELECT
+    t0.block_number,
+    t0.block_timestamp,
+    t0.tx_hash,
+    origin_function_signature,
+    origin_from_address,
+    origin_to_address,
+    t0.contract_address,
+    t0.pool_id,
+    event_name,
+    token_in,
+    p1.is_verified AS token_in_is_verified,
+    c1.token_decimals AS decimals_in,
+    c1.token_symbol AS symbol_in,
+    amount_in_unadj,
+    CASE
+      WHEN c1.token_decimals IS NULL THEN amount_in_unadj
+      ELSE (amount_in_unadj / pow(10, c1.token_decimals))
+    END AS amount_in_heal,
+    CASE
+      WHEN c1.token_decimals IS NOT NULL THEN amount_in_heal * p1.price
+      ELSE NULL
+    END AS amount_in_usd_heal,
+    token_out,
+    p2.is_verified AS token_out_is_verified,
+    c2.token_decimals AS decimals_out,
+    c2.token_symbol AS symbol_out,
+    amount_out_unadj,
+    CASE
+      WHEN c2.token_decimals IS NULL THEN amount_out_unadj
+      ELSE (amount_out_unadj / pow(10, c2.token_decimals))
+    END AS amount_out_heal,
+    CASE
+      WHEN c2.token_decimals IS NOT NULL THEN amount_out_heal * p2.price
+      ELSE NULL
+    END AS amount_out_usd_heal,
+    CASE
+      WHEN lp.pool_name IS NULL THEN CONCAT(
+        LEAST(
+          COALESCE(
+            c1.token_symbol,
+            CONCAT(SUBSTRING(token_in, 1, 5), '...', SUBSTRING(token_in, 39, 42))
+          ),
+          COALESCE(
+            c2.token_symbol,
+            CONCAT(SUBSTRING(token_out, 1, 5), '...', SUBSTRING(token_out, 39, 42))
+          )
+        ),
+        '-',
+        GREATEST(
+          COALESCE(
+            c1.token_symbol,
+            CONCAT(SUBSTRING(token_in, 1, 5), '...', SUBSTRING(token_in, 39, 42))
+          ),
+          COALESCE(
+            c2.token_symbol,
+            CONCAT(SUBSTRING(token_out, 1, 5), '...', SUBSTRING(token_out, 39, 42))
+          )
+        )
+      )
+      ELSE lp.pool_name
+    END AS pool_name_heal,
+    sender,
+    tx_to,
+    event_index,
+    t0.platform,
+    t0.protocol,
+    t0.version,
+    t0.type,
+    t0._log_id,
+    t0._inserted_timestamp
+  FROM
+    {{ this }}
+    t0
+    LEFT JOIN contracts
+    c1
+    ON t0.token_in = c1.contract_address
+    LEFT JOIN contracts
+    c2
+    ON t0.token_out = c2.contract_address
+    LEFT JOIN prices
+    p1
+    ON t0.token_in = p1.token_address
+    AND DATE_TRUNC(
+      'hour',
+      block_timestamp
+    ) = p1.hour
+    LEFT JOIN prices
+    p2
+    ON t0.token_out = p2.token_address
+    AND DATE_TRUNC(
+      'hour',
+      block_timestamp
+    ) = p2.hour
+    LEFT JOIN {{ ref('silver_dex__complete_dex_liquidity_pools') }}
+    lp
+    ON t0.contract_address = lp.pool_address
+    AND t0.pool_id = lp.pool_id
+  WHERE
+    CONCAT(
+      t0.block_number,
+      '-',
+      t0.platform,
+      '-',
+      t0.version
+    ) IN (
+      SELECT
+        CONCAT(
+          t1.block_number,
+          '-',
+          t1.platform,
+          '-',
+          t1.version
+        )
+      FROM
+        {{ this }}
+        t1
+      WHERE
+        t1.decimals_in IS NULL
+        AND t1._inserted_timestamp < (
+          SELECT
+            MAX(
+              _inserted_timestamp
+            ) - INTERVAL '{{ var("LOOKBACK", "4 hours") }}'
+          FROM
+            {{ this }}
+        )
+        AND EXISTS (
+          SELECT
+            1
+          FROM
+            contracts C
+          WHERE
+            C._inserted_timestamp > DATEADD('DAY', -14, SYSDATE())
+            AND C.token_decimals IS NOT NULL
+            AND C.contract_address = t1.token_in)
+          GROUP BY
+            1
+        )
+        OR CONCAT(
+          t0.block_number,
+          '-',
+          t0.platform,
+          '-',
+          t0.version
+        ) IN (
+          SELECT
+            CONCAT(
+              t2.block_number,
+              '-',
+              t2.platform,
+              '-',
+              t2.version
+            )
+          FROM
+            {{ this }}
+            t2
+          WHERE
+            t2.decimals_out IS NULL
+            AND t2._inserted_timestamp < (
+              SELECT
+                MAX(
+                  _inserted_timestamp
+                ) - INTERVAL '{{ var("LOOKBACK", "4 hours") }}'
+              FROM
+                {{ this }}
+            )
+            AND EXISTS (
+              SELECT
+                1
+              FROM
+                contracts C
+              WHERE
+                C._inserted_timestamp > DATEADD('DAY', -14, SYSDATE())
+                AND C.token_decimals IS NOT NULL
+                AND C.contract_address = t2.token_out)
+              GROUP BY
+                1
+            )
+            OR CONCAT(
+              t0.block_number,
+              '-',
+              t0.platform,
+              '-',
+              t0.version
+            ) IN (
+              SELECT
+                CONCAT(
+                  t3.block_number,
+                  '-',
+                  t3.platform,
+                  '-',
+                  t3.version
+                )
+              FROM
+                {{ this }}
+                t3
+              WHERE
+                t3.amount_in_usd IS NULL
+                AND t3._inserted_timestamp < (
+                  SELECT
+                    MAX(
+                      _inserted_timestamp
+                    ) - INTERVAL '{{ var("LOOKBACK", "4 hours") }}'
+                  FROM
+                    {{ this }}
+                )
+                AND EXISTS (
+                  SELECT
+                    1
+                  FROM
+                    prices
+                    p
+                  WHERE
+                    p._inserted_timestamp > DATEADD('DAY', -14, SYSDATE())
+                    AND p.price IS NOT NULL
+                    AND p.token_address = t3.token_in
+                    AND p.hour = DATE_TRUNC(
+                      'hour',
+                      t3.block_timestamp
+                    )
+                )
+              GROUP BY
+                1
+            )
+            OR CONCAT(
+              t0.block_number,
+              '-',
+              t0.platform,
+              '-',
+              t0.version
+            ) IN (
+              SELECT
+                CONCAT(
+                  t4.block_number,
+                  '-',
+                  t4.platform,
+                  '-',
+                  t4.version
+                )
+              FROM
+                {{ this }}
+                t4
+              WHERE
+                t4.amount_out_usd IS NULL
+                AND t4._inserted_timestamp < (
+                  SELECT
+                    MAX(
+                      _inserted_timestamp
+                    ) - INTERVAL '{{ var("LOOKBACK", "4 hours") }}'
+                  FROM
+                    {{ this }}
+                )
+                AND EXISTS (
+                  SELECT
+                    1
+                  FROM
+                    prices
+                    p
+                  WHERE
+                    p._inserted_timestamp > DATEADD('DAY', -14, SYSDATE())
+                    AND p.price IS NOT NULL
+                    AND p.token_address = t4.token_out
+                    AND p.hour = DATE_TRUNC(
+                      'hour',
+                      t4.block_timestamp
+                    )
+                )
+              GROUP BY
+                1
+            )
+            OR     
+            CONCAT(
+              t0.block_number,
+              '-',
+              t0.platform,
+              '-',
+              t0.version
+            ) IN (
+                select concat(
+                  t5.block_number,
+                  '-',
+                  t5.platform,
+                  '-',
+                  t5.version
+                )
+                from {{ this }} t5
+                where t5.token_in in (
+                  select token_address
+                  from {{ ref('price__ez_asset_metadata') }}
+                  where ifnull(is_verified_modified_timestamp, '1970-01-01' :: TIMESTAMP) > dateadd('day', -10, SYSDATE())
+                )
+              )
+            OR concat(
+              t0.block_number,
+              '-',
+              t0.platform,
+              '-',
+              t0.version
+            ) IN (  
+              select concat(
+                t6.block_number,
+                '-',
+                t6.platform,
+                '-',
+                t6.version
+              )
+              from {{ this }} t6
+              where t6.token_out in (
+                select token_address
+                from {{ ref('price__ez_asset_metadata') }}
+                where ifnull(is_verified_modified_timestamp, '1970-01-01' :: TIMESTAMP) > dateadd('day', -10, SYSDATE())
+              )
+            )
+        ),
+      {% endif %}
+
+      FINAL AS (
+        SELECT
+          *
+        FROM
+          complete_dex_swaps
+
+{% if is_incremental() and var(
+  'HEAL_MODEL'
+) %}
+UNION ALL
+SELECT
+  block_number,
+  block_timestamp,
+  tx_hash,
+  origin_function_signature,
+  origin_from_address,
+  origin_to_address,
+  contract_address,
+  pool_id,
+  event_name,
+  token_in,
+  token_in_is_verified,
+  decimals_in,
+  symbol_in,
+  amount_in_unadj,
+  amount_in_heal AS amount_in,
+  amount_in_usd_heal AS amount_in_usd,
+  token_out,
+  token_out_is_verified,
+  decimals_out,
+  symbol_out,
+  amount_out_unadj,
+  amount_out_heal AS amount_out,
+  amount_out_usd_heal AS amount_out_usd,
+  pool_name_heal AS pool_name,
+  sender,
+  tx_to,
+  event_index,
+  platform,
+  protocol,
+  version,
+  type,
+  _log_id,
+  _inserted_timestamp
+FROM
+  heal_model
+{% endif %}
+)
+SELECT
+  block_number,
+  block_timestamp,
+  tx_hash,
+  origin_function_signature,
+  origin_from_address,
+  origin_to_address,
+  contract_address,
+  pool_name,
+  pool_id,
+  event_name,
+  amount_in_unadj,
+  amount_in,
+  amount_in_usd,
+  amount_out_unadj,
+  amount_out,
+  amount_out_usd,
+  sender,
+  tx_to,
+  event_index,
+  platform,
+  protocol,
+  version,
+  type,
+  token_in,
+  IFNULL(
+    token_in_is_verified,
+    FALSE
+  ) AS token_in_is_verified,
+  token_out,
+  IFNULL(
+    token_out_is_verified,
+    FALSE
+  ) AS token_out_is_verified,
+  symbol_in,
+  symbol_out,
+  decimals_in,
+  decimals_out,
+  _log_id,
+  _inserted_timestamp,
+  {{ dbt_utils.generate_surrogate_key(
+    ['tx_hash','event_index']
+  ) }} AS complete_dex_swaps_id,
+  SYSDATE() AS inserted_timestamp,
+  SYSDATE() AS modified_timestamp,
+  '{{ invocation_id }}' AS _invocation_id
+FROM
+  FINAL qualify (ROW_NUMBER() over (PARTITION BY _log_id
+ORDER BY
+  _inserted_timestamp DESC)) = 1
