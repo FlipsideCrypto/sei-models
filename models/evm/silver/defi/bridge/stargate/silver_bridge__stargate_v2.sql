16a3c093b478bff2559e8d9501d4eef0f9115016
sam <sam@flipsidecrypto.com>
2025-08-11 12:01:23 +0800

    partial stargate

---
+++ models/evm/silver/defi/bridge/stargate/silver_bridge__stargate_v2.sql
@@ -0,0 +1,236 @@
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy = 'delete+insert',
+    unique_key = "block_number",
+    tags = ['noncore']
+) }}
+
+WITH bus_driven AS (
+
+    SELECT
+        block_number,
+        block_timestamp,
+        origin_from_address,
+        origin_to_address,
+        origin_function_signature,
+        tx_hash,
+        event_index,
+        event_name,
+        stargate_adapter_address AS bridge_address,
+        guid,
+        ticket_id,
+        from_address AS sender,
+        dst_receiver_address AS receiver,
+        dst_receiver_address AS destination_chain_receiver,
+        dst_chain_id,
+        dst_chain_id :: STRING AS destination_chain_id,
+        dst_chain AS destination_chain,
+        asset_address AS token_address,
+        NULL AS token_symbol,
+        amount_sent AS amount_unadj,
+        src_chain_id,
+        src_chain,
+        asset_id,
+        asset_name,
+        payload,
+        tx_type,
+        nonce,
+        sender_contract_address,
+        receiver_contract_address,
+        message_type,
+        protocol,
+        version,
+        TYPE,
+        platform,
+        CONCAT(
+            tx_hash,
+            '-',
+            event_index,
+            '-',
+            ticket_id
+        ) AS _log_id,
+        modified_timestamp
+    FROM
+        {{ ref('silver_bridge__stargate_v2_busdriven') }}
+
+{% if is_incremental() %}
+WHERE
+    modified_timestamp >= (
+        SELECT
+            MAX(modified_timestamp) - INTERVAL '12 hours'
+        FROM
+            {{ this }}
+    )
+{% endif %}
+),
+oft_sent AS (
+    SELECT
+        block_number,
+        block_timestamp,
+        origin_from_address,
+        origin_to_address,
+        origin_function_signature,
+        tx_hash,
+        event_index,
+        'OFTSent' AS event_name,
+        contract_address AS bridge_address,
+        guid,
+        NULL AS ticket_id,
+        from_address AS sender,
+        to_address AS receiver,
+        to_address AS destination_chain_receiver,
+        dst_chain_id,
+        dst_chain_id :: STRING AS destination_chain_id,
+        dst_chain AS destination_chain,
+        token_address,
+        NULL AS token_symbol,
+        amount_sent AS amount_unadj,
+        src_chain_id,
+        src_chain,
+        asset_id,
+        asset_name,
+        payload,
+        tx_type,
+        nonce,
+        sender_contract_address,
+        receiver_contract_address,
+        message_type,
+        protocol,
+        version,
+        TYPE,
+        platform,
+        CONCAT(
+            tx_hash,
+            '-',
+            event_index
+        ) AS _log_id,
+        modified_timestamp
+    FROM
+        {{ ref('silver_bridge__stargate_v2_oft') }}
+
+{% if is_incremental() %}
+WHERE
+    modified_timestamp >= (
+        SELECT
+            MAX(modified_timestamp) - INTERVAL '12 hours'
+        FROM
+            {{ this }}
+    )
+{% endif %}
+),
+combined AS (
+    SELECT
+        *
+    FROM
+        bus_driven
+    UNION ALL
+    SELECT
+        *
+    FROM
+        oft_sent
+),
+FINAL AS (
+    SELECT
+        *
+    FROM
+        combined
+
+{% if is_incremental() and 'stargate_heal' in vars.curated_fr_models %}
+UNION ALL
+SELECT
+    block_number,
+    block_timestamp,
+    origin_from_address,
+    origin_to_address,
+    origin_function_signature,
+    tx_hash,
+    event_index,
+    event_name,
+    bridge_address,
+    guid,
+    ticket_id,
+    sender,
+    receiver,
+    destination_chain_receiver,
+    dst_chain_id,
+    destination_chain_id,
+    LOWER(
+        b.chain
+    ) AS destination_chain,
+    A.address AS token_address,
+    A.asset AS token_symbol,
+    amount_unadj,
+    src_chain_id,
+    src_chain,
+    A.id AS asset_id,
+    A.asset AS asset_name,
+    payload,
+    tx_type,
+    nonce,
+    sender_contract_address,
+    receiver_contract_address,
+    message_type,
+    protocol,
+    version,
+    TYPE,
+    platform,
+    _log_id,
+    modified_timestamp
+FROM
+    {{ this }}
+    t
+    LEFT JOIN {{ ref('silver_bridge__stargate_v2_asset_seed') }} A
+    ON t.bridge_address = A.oftaddress
+    AND A.chain = 'sei'
+    LEFT JOIN {{ ref('silver_bridge__layerzero_v2_bridge_seed') }}
+    b
+    ON t.destination_chain_id = b.eid
+WHERE
+    t.token_address IS NULL
+    OR t.destination_chain IS NULL
+{% endif %}
+)
+SELECT
+    block_number,
+    block_timestamp,
+    origin_from_address,
+    origin_to_address,
+    origin_function_signature,
+    tx_hash,
+    event_index,
+    event_name,
+    bridge_address,
+    guid,
+    ticket_id,
+    sender,
+    receiver,
+    destination_chain_receiver,
+    dst_chain_id,
+    destination_chain_id,
+    destination_chain,
+    token_address,
+    token_symbol,
+    amount_unadj,
+    src_chain_id,
+    src_chain,
+    asset_id,
+    asset_name,
+    payload,
+    tx_type,
+    nonce,
+    sender_contract_address,
+    receiver_contract_address,
+    message_type,
+    protocol,
+    version,
+    TYPE,
+    platform,
+    _log_id,
+    modified_timestamp
+FROM
+    FINAL qualify ROW_NUMBER() over (
+        PARTITION BY _log_id
+        ORDER BY
+            modified_timestamp DESC,
+            destination_chain DESC nulls last
+    ) = 1
